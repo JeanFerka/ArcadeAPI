fc550a982f85bd490e7a5d3b0331e2655d138b0f
Jean Ferkatadji <jean.ferkatadji@epitech.eu>
2025-03-18 23:44:53 +0100

    feat(Interface): add IGameModule interface

---
+++ Arcade/IGameModule.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2025
+** Arcade
+** File description:
+** IGameModule.hpp
+*/
+
+#ifndef ARCADE_IGAMEMODULE_HPP
+    #define ARCADE_IGAMEMODULE_HPP
+
+    #include "IDrawable.hpp"
+
+namespace Arcade {
+    class IGameModule;
+}
+
+typedef Arcade::IGameModule ArcadeIGameModule
+
+namespace Arcade {
+    class IGameModule {
+        public:
+            virtual ~IGameModule() = default;
+
+            virtual void update(std::vector<Event> events) = 0;
+
+            virtual std::vector<IDrawable> getDrawables() = 0;
+    }
+}
+
+#endif /* ARCADE_IGAMEMODULE_HPP */
