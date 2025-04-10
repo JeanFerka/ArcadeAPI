f52caa37ad4c91a5dc3a882e5fbdb9ca83586eaf
Maxence Vancon <maxence.vancon@epitech.eu>
2025-04-10 17:56:52 +0200

    feat(Architecture): change name for the Rect

--- Arcade/Rect.hpp
+++ Arcade/Rect.hpp
@@ -55,14 +55,14 @@ namespace Arcade {
     class Rect3d {
         public:
             Rect3d() = default;
-            Rect3d(T X, T Y, T Z, T Width, T Height, T Lenght)
+            Rect3d(T X, T Y, T Z, T Width, T Height, T Depth)
             {
                 this->x = X;
                 this->y = Y;
                 this->z = Z;
                 this->width = Width;
                 this->height = Height;
-                this->lenght = Lenght;
+                this->depth = Depth;
             };
             template <typename U>
             Rect3d(const Rect3d<U> &rectangle)
@@ -72,24 +72,24 @@ namespace Arcade {
                 this->z = rectangle.z;
                 this->width = rectangle.width;
                 this->height = rectangle.height;
-                this->Lenght = rectangle.lenght;
+                this->Depth = rectangle.depth;
             }
             ~Rect3d() = default;
         public:
             bool contains(const Vector3<T> &point) const
             {
                 return this->x < point.x && point.x < this->x + this->width
-                    && this->y < point.y && point.y < this->y + this->lenght
+                    && this->y < point.y && point.y < this->y + this->depth
                     && this->z < point.z && point.z < this->z + this->height;
 
             }
             bool intersects(const Rect3d<T> &rectangle) const
             {
                 return !(this->x + this->width <= rectangle.x
-                    || this->y + this->lenght <= rectangle.y
+                    || this->y + this->depth <= rectangle.y
                     || this->z + this->height <= rectangle.z
                     || rectangle.x + rectangle.width <= this->x
-                    || rectangle.y + rectangle.lenght <= this->y
+                    || rectangle.y + rectangle.depth <= this->y
                     || rectangle.z + rectangle.height <= this->z);
             }
         public:
@@ -98,7 +98,7 @@ namespace Arcade {
             T z;
             T width;
             T height;
-            T Lenght;
+            T depth;
     };
 }
 #endif /* ARCADE_RECT_HPP */
